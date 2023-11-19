3756cbb1744432d632b0537d5dfaefef90c64fea
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2023-11-19 14:37:24 +1300

    feat: fully cutover to gorilla websockets. Server now responds to client with clients message and a random string

--- main.go
+++ main.go
@@ -16,89 +16,6 @@ import (
 	"net/http"
 )
 
-func hello(c echo.Context) error {
-
-	var upgrader = websocket.Upgrader{}
-	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-	if err != nil {
-		return err
-	}
-	defer ws.Close()
-
-	for {
-		mt, message, err := ws.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			return err
-		}
-		log.Printf("recv: %s", message)
-		message = []byte("hello")
-
-		randomString := random.String(10)
-		var b bytes.Buffer
-		//html := fmt.Sprintf("<ul hx-swap-oob='beforeend:#items'><li>%s</li></ul>", randomString)
-		w := io.Writer(&b)
-		err = templates.ListItem(randomString).Render(c.Request().Context(), w)
-		if err != nil {
-			c.Logger().Error("failed to template ListItem")
-		}
-		html := b.Bytes()
-
-		err = ws.WriteMessage(mt, html)
-		if err != nil {
-			log.Println("write:", err)
-			return err
-		}
-	}
-	//
-	//websocket.Handler(func(ws *websocket.Conn) {
-	//	defer ws.Close()
-	//	for {
-	//
-	//		// Write
-	//		randomString := random.String(10)
-	//
-	//		// Partial Attempt
-	//		// Return a list item wrapped in a list in order to mitigate interesting parsing behaviour when using OOB fragments
-	//		// https://github.com/bigskysoftware/htmx/issues/1198#issuecomment-1763180864
-	//		// May in time be resolved by
-	//		var b bytes.Buffer
-	//		//html := fmt.Sprintf("<ul hx-swap-oob='beforeend:#items'><li>%s</li></ul>", randomString)
-	//		w := io.Writer(&b)
-	//		err := templates.ListItem(randomString).Render(c.Request().Context(), w)
-	//		if err != nil {
-	//			c.Logger().Error("failed to template ListItem")
-	//		}
-	//		html := b.String()
-	//
-	//		err = websocket.Message.Send(ws, html)
-	//		if err != nil {
-	//			if errors.Is(err, syscall.EPIPE) {
-	//				c.Logger().Error("connection broken")
-	//				return
-	//			}
-	//			c.Logger().Error(err)
-	//		}
-	//
-	//		//// Read
-	//		//msg := ""
-	//		//err = websocket.Message.Receive(ws, &msg)
-	//		//if err != nil {
-	//		//	if errors.Is(err, io.EOF) {
-	//		//		log.Debug("No data to read")
-	//		//	} else if errors.Is(err, syscall.EPIPE) {
-	//		//		log.Debug("broken pipe")
-	//		//	}
-	//		//	c.Logger().Error(err)
-	//		//}
-	//
-	//		//fmt.Printf("%s\n", msg)
-	//		time.Sleep(3 * time.Second)
-	//	}
-	//}).ServeHTTP(c.Response(), c.Request())
-	//return nil
-}
-
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
@@ -127,18 +44,21 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		// Print received message
 		log.Printf("Received: %s\n", msg)
 
-		// Write message back to the client
-		if err = conn.WriteMessage(websocket.TextMessage, msg); err != nil {
-			log.Println("Error writing message:", err)
-			break
+		randomString := fmt.Sprintf("Server says: %s", random.String(10))
+		var b bytes.Buffer
+		w := io.Writer(&b)
+
+		// Write received msg back to user
+		err = templates.ListItem(fmt.Sprintf("You said: %s", msg)).Render(context.Background(), w)
+		err = conn.WriteMessage(websocket.TextMessage, b.Bytes())
+		if err != nil {
+			return
 		}
 
-		randomString := random.String(10)
-		var b bytes.Buffer
-		//html := fmt.Sprintf("<ul hx-swap-oob='beforeend:#items'><li>%s</li></ul>", randomString)
-		w := io.Writer(&b)
-		err = templates.ListItem(randomString).Render(context.Background(), w)
-		conn.WriteMessage(websocket.TextMessage, b.Bytes())
+		// Write a new server message back to the user
+		b.Reset()
+		err = templates.ListItem(fmt.Sprintf(randomString)).Render(context.Background(), w)
+		err = conn.WriteMessage(websocket.TextMessage, b.Bytes())
 		if err != nil {
 			return
 		}
@@ -151,12 +71,11 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	//e.GET("/test", test)
-
+	// Index
 	e.GET("/", func(c echo.Context) error {
 		return templates.Index().Render(c.Request().Context(), c.Response().Writer)
 	})
-	//e.GET("/ws", hello)
+
 	// WebSocket endpoint
 	e.GET("/ws", func(c echo.Context) error {
 		wsHandler(c.Response(), c.Request())
